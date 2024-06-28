55ff3759df45a903b815b2b6e1bf9d17f963a53c
Gaspard FEREY <gaspard.ferey@arcep.fr>
2024-06-28 17:55:16 +0200

    Nettoyage du chargement des donnees

--- analysis/data.py
+++ analysis/data.py
@@ -1,37 +1,71 @@
 # -*- coding: utf-8 -*-
-
-
 import json, zipfile
 import pandas as pd
-
-def load_data(file):
-    with zipfile.ZipFile(file, "r") as z:
-        for filename in z.namelist():  
-            with z.open(filename) as f:  
-                return json.loads(f.read())
-            
+import numpy as np
 
 birth = '2024-05-03 09:58:00'
 birth_datetime = pd.to_datetime(birth)
 
 now = pd.Timestamp.now().tz_localize(None).ceil(freq='s')
 
+all_babykeys = ['sleep', 'nursingfeed', 'bottlefeed', 'nappy']
+
 def parse_datetime(d):
     res = pd.to_datetime(d).tz_localize(None).ceil(freq='s')
     return now if res.year == 1970 else res
 
-def get_sleep_min_df(data):
-    sleep = data['baby_sleep']
-    last_date = parse_datetime(sleep[0]['endDate'])
-    datetimes = pd.date_range(start=birth, end=last_date, freq='min')
-    df = pd.DataFrame({'datetime': datetimes})
+def get_json_from_zip_file(file):
+    with zipfile.ZipFile(file, "r") as z:
+        for filename in z.namelist():  
+            with z.open(filename) as f:  
+                return json.loads(f.read())
+            
+def get_minute_df_from_json(data):
+    sleep       = data['baby_sleep']
+    nursingfeed = data['baby_nursingfeed']
+    bottlefeed  = data['baby_bottlefeed']
+    nappy       = data['baby_nappy']
+    
+    last_sleep       = parse_datetime(      sleep[0]['endDate'])
+    last_nursingfeed = parse_datetime(nursingfeed[0]['endDate'])
+    last_bottlefeed  = parse_datetime( bottlefeed[0]['date'])
+    last_nappy       = parse_datetime(      nappy[0]['date'])
+    
+        
+    last_date = max(last_sleep, last_nursingfeed, last_bottlefeed, last_nappy)
+    daterange= pd.date_range(start=birth, end=last_date, freq='min')
+    
+    # Init dataframe with datetime index
+    df = pd.DataFrame({'datetime': daterange})
     df.set_index('datetime', inplace=True, drop=False)
-    df['sleep'] = sum( [ (df['datetime'] >= parse_datetime(s['startDate'])) & (df['datetime'] < parse_datetime(s['endDate'])) for s in sleep ] )
     df['date'] = df['datetime'].dt.date
     df['time'] = df['datetime'].dt.time
+    
+    # Actual data
+    df['sleep']       = sum( [ (df['datetime'] >= parse_datetime(e['startDate'])) & (df['datetime'] < parse_datetime(e['endDate'])) for e in sleep ] )
+    df['nursingfeed'] = sum( [ (df['datetime'] >= parse_datetime(e['startDate'])) & (df['datetime'] < parse_datetime(e['endDate'])) for e in nursingfeed ] )
+    df['bottlefeed']  = sum( [ (df['datetime'] == parse_datetime(e['date'])) *  e['amountML'] for e in bottlefeed ] )
+    df['nappy']       = sum( [ (df['datetime'] == parse_datetime(e['date'])) for e in nappy ] )
     return df
 
 
+def get_hour_df_from_minute_df(mdf):
+    df = mdf.groupby( mdf.datetime.dt.floor('h') ).agg({
+        'sleep'      : lambda group: np.sum(group) / 60,
+        'nursingfeed': np.sum,
+        'bottlefeed' : np.sum,
+        'nappy'      : np.sum,
+        })
+    # Return the dataframe of all entries except for the last available hour
+    # which is most likely incomplete
+    return df[:-1]
+
+
+# Pretty printing function to print 1h intervals using their bounds
 def pp_timeslot(t, delta='1h'):
     return "[{} - {}]".format(t,t+pd.to_timedelta(delta))
+
+
+def add_shifted_to_hour_df(df, nb_shifts):
+    return pd.concat({ f'{k}_{i}': df[k].shift(i) for k in all_babykeys for i in range(nb_shifts+1) }, axis=1)
 
--- analysis/model.py
+++ analysis/model.py
@@ -1,40 +1,54 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 23 10:37:04 2024
 
-@author: PC
-"""
-
-# -*- coding: utf-8 -*-
-
-extract_file = r"../babyplus_data_export (2).zip"
+extract_file = r"..\data\babyplus_data_export.zip"
 nb_predictions = 10
 
-from data import birth_datetime, load_data, get_sleep_min_df, pp_timeslot
+###############################################################################
+##########               Loading libraries and data                  ##########
+###############################################################################
+
+from data import (
+    add_shifted_to_hour_df,
+    all_babykeys,
+    get_hour_df_from_minute_df,
+    get_json_from_zip_file,
+    get_minute_df_from_json,
+    pp_timeslot,
+    )
 import pandas as pd
 import numpy as np
 
-data = load_data(extract_file)
-sleep = get_sleep_min_df(data)
+jsondata = get_json_from_zip_file(extract_file)
+mdf = get_minute_df_from_json(jsondata)
+hdf = get_hour_df_from_minute_df(mdf)
 
 
-start_date = '2024-06-22 10:00:00'
+###############################################################################
+##########                         Linear Tree                       ##########
+###############################################################################
+# Requires :
+#    ```sh
+#    pip install linear-tree
+#    ```
 
-start_date = birth_datetime.ceil('h')+pd.to_timedelta('48h')
+# Predict value at t from the [history_depth] previous values : t-1, t-2, ... t-[history_depth]
+history_depth = 48
 
-hsleep = sleep.groupby( sleep.datetime.dt.floor('h') ).agg({'sleep':'sum'}) / 60
+# Generate extra columns for history
+df = add_shifted_to_hour_df(hdf, history_depth)
 
-
-df = hsleep.join(hsleep.shift(range(1,48)))
-
-dt = df.index
-filt = (dt > start_date) & (dt < dt.max())
-
+# Exclude first [history_depth] entries : history is not fully available
+df = df[history_depth:]
 
 
-learn_dataset = df[filt].sample(frac=1)
-X = learn_dataset.drop('sleep', axis=1).to_numpy()
-y = learn_dataset['sleep'].to_numpy()
+# Shuffling dataset (optional)
+learn_dataset = df.sample(frac=1)
+
+# Input vectors : remove values at time t (keep only strict history : t-1, t-2, etc.)
+X = learn_dataset.drop([f'{k}_0' for k in all_babykeys], axis=1).to_numpy()
+
+# Output vector (amount of sleep at time t)
+y = learn_dataset['sleep_0'].to_numpy()
 
 
 print("Learning...")
@@ -48,13 +62,13 @@ regr.fit(X, y)
 
 print("Predictions:")
 
+# We start our predictions after the latest available time
+predict_time = hdf.index.max()
 
-predict_time = dt.max()
-predict_x = df[dt == predict_time].drop('sleep', axis=1).to_numpy()
+predict_x = df[df.index == predict_time].drop([f'{k}_0' for k in all_babykeys], axis=1).to_numpy()
 for p in range(nb_predictions):
-    print(f"{pp_timeslot(predict_time)}  ->  {regr.predict(predict_x)[0]}")
     predict_time = predict_time + pd.to_timedelta('1h')
+    print(f"{pp_timeslot(predict_time)}  ->  {regr.predict(predict_x)[0]}")
     predict_x = np.roll(predict_x, 1)
     predict_x.flat[0] = 0
-
 
